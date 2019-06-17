8b04f514a2ab257692f56dea4bad5d9b55ea2b03
Resender36 <dmitrym36@gmail.com>
2019-06-18 01:22:03 +0300

    commit 18/06/2019 01:21

--- src/BeatBox.java
+++ src/BeatBox.java
@@ -2,6 +2,8 @@ import javax.sound.midi.Sequence;
 import javax.sound.midi.Sequencer;
 import javax.sound.midi.Track;
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class BeatBox {
@@ -21,7 +23,41 @@ public class BeatBox {
     }
 
     public void buildGUI(){
-        
+        theFrame = new JFrame("Cyber BeatBox") ;
+        theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        BorderLayout layout = new BorderLayout() ;
+        JPanel background = new JPanel(layout) ;
+        background.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+
+        checkboxList = new ArrayList<JCheckBox>() ;
+        Box buttonBox = new Box(BoxLayout.Y_AXIS) ;
+
+        JButton start = new JButton("Start") ;
+        start.addActionListener(new MyStartListener());
+        buttonBox.add(start) ;
+
+        JButton stop = new JButton("Stop") ;
+        stop.addActionListener(new MyStopListener());
+        buttonBox.add(stop) ;
+
+        JButton upTempo = new JButton("Tempo up!") ;
+        upTempo.addActionListener(new MyUpTempoListener());
+        buttonBox.add(upTempo) ;
+
+        JButton downTempo = new JButton("Tempo down!") ;
+        downTempo.addActionListener(new MyDownTempoListener());
+        buttonBox.add(downTempo) ;
+
+        Box nameBox = new Box(BoxLayout.Y_AXIS) ;
+        for (int i = 1; i < 16; i++)
+            nameBox.add(new Label(instrumentNames[i])) ;
+
+        background.add(BorderLayout.EAST, buttonBox) ;
+        background.add(BorderLayout.WEST, nameBox) ;
+
+        theFrame.getContentPane().add(background) ;
+
+        GridLayout grid = new GridLayout(16,16) ;
     }
 
 }
